a6eb3f2b2901103f79c02e1829b9b7a39badc032
Uwqan <zlayaribka@gmail.com>
2024-02-14 12:28:31 +0400

    start

---
+++ AiWorkshop/src/test/java/com/aiworkshop/AiWorkShop/AiWorkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiworkshop.AiWorkShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWorkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
