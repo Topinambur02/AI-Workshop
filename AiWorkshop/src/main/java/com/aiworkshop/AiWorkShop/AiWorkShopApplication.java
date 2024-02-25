3c4c476011949766ba0c0bbc92de0251b70e1ff6
Orex-x <orexkashtan@gmail.com>
2024-02-25 18:59:51 +0300

    выложил в прод первую версию

--- AiWorkshop/src/main/java/com/aiworkshop/AiWorkShop/AiWorkShopApplication.java
+++ AiWorkshop/src/main/java/com/aiworkshop/AiWorkShop/AiWorkShopApplication.java
@@ -2,9 +2,10 @@ package com.aiworkshop.AiWorkShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AiWorkShopApplication {
+public class AiWorkShopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiWorkShopApplication.class, args);
