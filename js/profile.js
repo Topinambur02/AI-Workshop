1d211d2fe78f793f7e2acc60db80a57316e7840a
PeaceOfMaster <kirillmaster2020@gmail.com>
2024-03-18 16:34:19 +0300

    Добавил анимацию и все переключения вкладок профиля
    Пока кроме сохраненных других вкладок нет поэтому на деле переключение не происходит

--- js/profile.js
+++ js/profile.js
@@ -1,11 +1,78 @@
 // ELEMENTS AND VARIABLES
 const change_profile_info = document.getElementById('profile_change_profile_info')
+const current_profile_tab = document.getElementsByClassName('tab_button_active')
+const saved_tab = document.getElementById('saved_tab')
+const liked_tab = document.getElementById('liked_tab')
+const history_tab = document.getElementById('history_tab')
+const comments_tab = document.getElementById('comments_tab')
 
 
 // EVENT LISTENERS
 change_profile_info.addEventListener('click', goToEditProfile)
 
+saved_tab.addEventListener('click', savedTab)
+liked_tab.addEventListener('click', likedTab)
+history_tab.addEventListener('click', historyTab)
+comments_tab.addEventListener('click', commentsTab)
 
 
 // FUNCTIONS
 function goToEditProfile() {window.location.href = 'edit_profile.html'}
+
+function savedTab() {
+    // Activate tab animation
+    const current_profile_tab = document.getElementsByClassName('tab_button_active')[0]
+    const current_profile_tab_underline = current_profile_tab.querySelector('hr')
+    
+    current_profile_tab_underline.style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    current_profile_tab_underline.style.width = '0%'
+    setTimeout(() => (
+        current_profile_tab.classList.remove('tab_button_active')
+    ), 250)
+    saved_tab.classList.add('tab_button_active')
+    saved_tab.querySelector('hr').style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    saved_tab.querySelector('hr').style.width = '100%'
+}
+function likedTab() {
+    // Activate tab animation
+    const current_profile_tab = document.getElementsByClassName('tab_button_active')[0]
+    const current_profile_tab_underline = current_profile_tab.querySelector('hr')
+    
+    current_profile_tab_underline.style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    current_profile_tab_underline.style.width = '0%'
+    setTimeout(() => (
+        current_profile_tab.classList.remove('tab_button_active')
+    ), 250)
+    liked_tab.classList.add('tab_button_active')
+    liked_tab.querySelector('hr').style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    liked_tab.querySelector('hr').style.width = '100%'
+
+}
+function historyTab() {
+    // Activate tab animation
+    const current_profile_tab = document.getElementsByClassName('tab_button_active')[0]
+    const current_profile_tab_underline = current_profile_tab.querySelector('hr')
+    
+    current_profile_tab_underline.style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    current_profile_tab_underline.style.width = '0%'
+    setTimeout(() => (
+        current_profile_tab.classList.remove('tab_button_active')
+    ), 250)
+    history_tab.classList.add('tab_button_active')
+    history_tab.querySelector('hr').style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    history_tab.querySelector('hr').style.width = '100%'
+}
+function commentsTab() {
+    // Activate tab animation
+    const current_profile_tab = document.getElementsByClassName('tab_button_active')[0]
+    const current_profile_tab_underline = current_profile_tab.querySelector('hr')
+    
+    current_profile_tab_underline.style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    current_profile_tab_underline.style.width = '0%'
+    setTimeout(() => (
+        current_profile_tab.classList.remove('tab_button_active')
+    ), 250)
+    comments_tab.classList.add('tab_button_active')
+    comments_tab.querySelector('hr').style.transition = 'width 0.3s cubic-bezier(0.65, 0.01, 0.41, 1.02) 0s'
+    comments_tab.querySelector('hr').style.width = '100%'
+}
